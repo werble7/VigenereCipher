f4cc7b53cdde0f47d8e9e7b244c61c983a2d85ad
werble7 <60023966+werble7@users.noreply.github.com>
2023-04-20 12:40:42 -0300

    Adição de atacante
    Atacante que resume-se a parte 2 do trabalho, decifrador sem necessidade de chave, porém com margem de erro

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,5 +1,5 @@
 # Alunos: Henrique Valente Lima 211055380
-#         ...
+#         Gabriel Brito Franca  211020867
 
 # Cifra de Vigenère
 
@@ -9,6 +9,123 @@
 #     decifrado segundo a cifra de Vigenère, recuperando uma mensagem.
 
 import re
+
+en_frequencies = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,
+                  0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,
+                  0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,
+                  0.00978, 0.02360, 0.00150, 0.01974, 0.00074]
+
+pt_frequencies = [0.1463, 0.0104, 0.0388, 0.0499, 0.1257, 0.0102, 0.0130, 0.0128,
+                  0.0618, 0.0040, 0.0002, 0.0278, 0.0474, 0.0505, 0.1073, 0.0252,
+                  0.012, 0.0653, 0.0781, 0.0434, 0.0463, 0.0167, 0.0001, 0.0021,
+                  0.0001, 0.0047]
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+coincidences = []
+
+
+def filter_string(string):
+    string = ''.join(x.lower() for x in string if x.isalpha())
+    return string
+
+
+def show_coincidences(cipherText):
+    counter = 0
+    cipherLength = len(cipherText)
+    copy = cipherText
+    for offset in range(1, cipherLength // 2):
+        j = 0
+        for i in range(offset, cipherLength):
+            if cipherText[i] == copy[j]:
+                counter += 1
+            j += 1
+        print(str(offset) + ": " + str(counter))
+        counter = 0
+
+
+def get_c(sequence):
+    N = float(len(sequence))
+    frequency_sum = 0.0
+    for letter in letters:
+        frequency_sum += sequence.count(letter) * (sequence.count(letter) - 1)
+    if N * (N - 1) <= 0:
+        index = frequency_sum / 1
+    else:
+        index = frequency_sum / (N * (N - 1))
+    return index * 26
+
+
+def probable_key_length(ciphertext, en):
+    print("-------------Chances for key lenght-------------")
+    for guess in range(21):
+        coincidence_sum = 0.0
+        for i in range(guess):
+            letter_sequence = ""
+            for j in range(0, len(ciphertext[i:]), guess):
+                letter_sequence += ciphertext[i + j]
+            coincidence_sum = get_c(letter_sequence)
+        coincidences.append(coincidence_sum)
+    print_coincidences(coincidences)
+
+
+def print_coincidences(coincidences_array):
+    for i in range(len(coincidences_array)):
+        print(str(i) + ": " + str(coincidences_array[i]))
+    print()
+
+
+def frequencies(seq, en):
+    chi_squared_array = [0] * 26
+    if en:
+        letter_frequencies = en_frequencies
+    else:
+        letter_frequencies = pt_frequencies
+    for i in range(26):
+        sum_sq = 0.0
+        sequence_offset = [chr(((ord(seq[j]) - 97 - i) % 26) + 97) for j in range(len(seq))]
+        c = [0] * 26
+        for l in sequence_offset:
+            c[ord(l) - ord('a')] += 1
+        for j in range(26):
+            if len(seq) > 0:
+                c[j] *= (1.0 / float(len(seq)))
+        for j in range(26):
+            sum_sq += ((c[j] - float(letter_frequencies[j])) ** 2) / float(letter_frequencies[j])
+        chi_squared_array[i] = sum_sq
+    shift = chi_squared_array.index(min(chi_squared_array))
+    return chr(shift + 97)
+
+
+def get_key(ciphertext, key_length, en):
+    key = ''
+    for i in range(int(key_length)):
+        sequence = ""
+        for j in range(0, len(ciphertext[i:]), int(key_length)):
+            sequence += ciphertext[i + j]
+        key += frequencies(sequence, en)
+    return key
+
+
+def attack():
+    language = input("Insert 'en' for english or 'pt' for portuguese: ")
+    en = True
+    if language == 'pt':
+        en = False
+    ciphertext_unfiltered = input("Insert the ciphed text: ")
+    ciphertext = filter_string(ciphertext_unfiltered)
+    probable_key_length(ciphertext, en)
+    '''
+    key_length_guess = input("Insert the key lenght: ")
+    vig.key = get_key(ciphertext, key_length_guess, en)
+    print("Possible key: " + vig.key)
+    print("Possible message:", vig.decipher(ciphertext), "\n")
+    '''
+    for x in range(len(coincidences)):
+        if coincidences[x] > 0:
+            print("key lenght:", x)
+            vig.key = get_key(ciphertext, x, en)
+            print("Possible key: " + vig.key)
+            print("Possible message:", vig.decipher(ciphertext), "\n")
 
 
 class Vigenere():
@@ -59,16 +176,16 @@ if __name__ == "__main__":
 
         if op == 1:
             vig.key = input('\nInsert a key: \n')
-            cifra_gerada = vig.encipher(input('\nInsert a message to be ciphed: \n'))
-            print("\ngenerated message:\n" + cifra_gerada + "\n")
+            cipher = vig.encipher(input('\nInsert a message to be ciphed: \n'))
+            print("\ngenerated message:\n" + cipher + "\n")
 
         elif op == 2:
             vig.key = input('\nInsert a key: \n')
-            msg_obtida = vig.decipher(list(input('\nInsert a ciphed message to be deciphed\n')))
-            print("\nObtained message:\n" + msg_obtida + "\n")
+            msg = vig.decipher(list(input('\nInsert a ciphed message to be deciphed\n')))
+            print("\nObtained message:\n" + msg + "\n")
 
         elif op == 3:
-            continue
+            attack()
 
         elif op == 4:
             break
