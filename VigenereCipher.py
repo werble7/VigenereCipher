a73344d00e15a4774b102a443af397014737be9f
werble7 <bravehenrique@gmail.com>
2023-05-04 09:56:23 -0300

    Simplificada a função de encrypt

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,39 +1,34 @@
 # Alunos: Henrique Valente Lima 211055380
 #         Gabriel Brito Franca  211020867
 
-def decrypt(ciphermsg, key):
-    ascii = []
-    cipher_ascii = [ord(letter) for letter in ciphermsg]
-    key_ascii = [ord(letter) for letter in key]
-
-    for i in range(len(cipher_ascii)):
-        ascii.append(((cipher_ascii[i] - key_ascii[i % len(key)]) % 26) + 97)
-
-    msg = ''.join(chr(i) for i in ascii)
-    return msg
-
-
-def encrypt(msg, key):
-    ascii = [ord(letter) for letter in msg]
-    key_ascii = [ord(letter) for letter in key]
+def encrypt(e_msg, e_key):
+    e_ascii = [ord(letter) for letter in e_msg]
+    key_ascii = [ord(letter) for letter in e_key]
     cipher_ascii = []
 
-    for i in range(len(ascii)):
+    for i in range(len(e_ascii)):
+        cipher_ascii.append(((e_ascii[i] + key_ascii[i % len(e_key)] - 194) % 26) + 97)
 
-        temp = ascii[i] - 97 + key_ascii[i % len(key)]
-        if temp <= 122:
-            cipher_ascii.append(temp)
-        else:
+    e_msg = ''.join(chr(i) for i in cipher_ascii)
+    return e_msg
 
-            cipher_ascii.append(temp - 26)
 
-    ciphermsg = ''.join(chr(i) for i in cipher_ascii)
-    return ciphermsg
+def decrypt(d_msg, d_key):
+    d_ascii = []
+    cipher_ascii = [ord(letter) for letter in d_msg]
+    key_ascii = [ord(letter) for letter in d_key]
+
+    for i in range(len(cipher_ascii)):
+        d_ascii.append(((cipher_ascii[i] - key_ascii[i % len(d_key)]) % 26) + 97)
+
+    d_msg = ''.join(chr(i) for i in d_ascii)
+    return d_msg
 
 
 if __name__ == '__main__':
     while True:
         op1 = int(input("1- Encrypt\n2- Decrypt\n3- Exit\n"))
+
         if op1 == 1:
             msg = input("Insert message to be encrypted: \n")
             key = input("Insert key to encrypt with: \n")
@@ -41,11 +36,11 @@ if __name__ == '__main__':
             msg = ''.join(x.lower() for x in msg if x.isalpha())
             key = ''.join(x.lower() for x in key if x.isalpha())
 
-            cipher = encrypt(msg, key)
-            print(f"result: {cipher}\n")
+            msg = encrypt(msg, key)
+            print(f"result: {msg}\n")
 
         elif op1 == 2:
-            msg = input("Insert ciphed message to be decrypted: \n")
+            msg = input("Insert cipher message to be decrypted: \n")
             key = input("Enter key to decrypt with: ")\
 
             msg = ''.join(x.lower() for x in msg if x.isalpha())
